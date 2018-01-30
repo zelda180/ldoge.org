92723b155d9847b7d2caa9b99e3a1fb77f87654f
suchapp <wow@suchapp.me>
2018-01-30 10:32:16 +0200

    currency converter

--- js/main.js
+++ js/main.js
@@ -1,61 +1,15 @@
-particlesJS.load('space', '/js/particlesjs-config.json');            
-
-$('.menu-toggle').click(function(e) {
-    e.stopPropagation();
-    $('html').toggleClass('is-menu-toggled-on');
-});    
-
-var rotate_words = $('.rotate-words');
-if(rotate_words.length) {
-    
-    var next_word_index = 0;
-    
-    if(Modernizr.csstransforms) {
-    
-        rotate_words.each(function(index, element) {
-            $(element).find('span').eq(0).addClass('active');
-            setInterval(function(){
-                next_word_index = $(element).find('.active').next().length ? $(element).find('.active').next().index() : 0;
-                $(element).find('.active').addClass('rotate-out').removeClass('rotate-in active');
-                $(element).find('span').eq(next_word_index).addClass('rotate-in active').removeClass('rotate-out');
-            },3000);
-        });
-
-    }
-    else {
-        
-        rotate_words.each(function(index, element) {
-            $(element).find('span').eq(0).addClass('active').show();
-            setInterval(function(){
-                next_word_index = $(element).find('.active').next().length ? $(element).find('.active').next().index() : 0;
-                $(element).find('.active').removeClass('active').slideUp(500);
-                $(element).find('span').eq(next_word_index).addClass('active').slideDown(500);
-            },3000);
-        });
-    }
-}
-
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-      }
-    }
-  });
-
 $(document).ready(function(){ 
+
+    /* Nav */
+    $('.menu-toggle').click(function(e) {
+        e.stopPropagation();
+        $('html').toggleClass('is-menu-toggled-on');
+    });    
+
+    /* Space */
+    particlesJS.load('space', '/js/particlesjs-config.json');            
+
+    /* Rocket animation */
     function loop() {
         $('#rocket').css({right:0,top:0});
         var rightMove=Math.floor(Math.random()*161)-80;
@@ -75,4 +29,76 @@ $(document).ready(function(){
         });
     }
     loop(); 
+
+    /* Intro */
+    var rotate_words = $('.rotate-words');
+    if(rotate_words.length) {
+        
+        var next_word_index = 0;
+        
+        if(Modernizr.csstransforms) {
+        
+            rotate_words.each(function(index, element) {
+                $(element).find('span').eq(0).addClass('active');
+                setInterval(function(){
+                    next_word_index = $(element).find('.active').next().length ? $(element).find('.active').next().index() : 0;
+                    $(element).find('.active').addClass('rotate-out').removeClass('rotate-in active');
+                    $(element).find('span').eq(next_word_index).addClass('rotate-in active').removeClass('rotate-out');
+                },3000);
+            });
+
+        }
+        else {
+            
+            rotate_words.each(function(index, element) {
+                $(element).find('span').eq(0).addClass('active').show();
+                setInterval(function(){
+                    next_word_index = $(element).find('.active').next().length ? $(element).find('.active').next().index() : 0;
+                    $(element).find('.active').removeClass('active').slideUp(500);
+                    $(element).find('span').eq(next_word_index).addClass('active').slideDown(500);
+                },3000);
+            });
+        }
+    }
+
+    /* Smooth scroll */
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+            }
+        }
+    });
+
+    /* Converter */
+    var price_local;
+    $.getJSON( "https://api.coinmarketcap.com/v1/ticker/litedoge/?convert=" + local_currency, function( data ) {
+        price_local = data && data[0] != undefined && data[0]['price_'+local_currency.toLowerCase()] != undefined ? data[0]['price_'+local_currency.toLowerCase()] : NaN;
+        convert_to_ldoge()
+    });
+
+    $('#currency_local_symbol').html(local_currency);
+
+    var convert_to_local = function() {
+        $('#converter_local').val($('#converter_ldoge').val()*price_local);
+    };
+
+    var convert_to_ldoge = function() {
+        $('#converter_ldoge').val(Math.round($('#converter_local').val()/price_local*100)/100);
+    };
+
+    $('#converter_ldoge').change(convert_to_local);
+    $('#converter_local').change(convert_to_ldoge);
+
+    $('#converter_ldoge').keyup(convert_to_local);
+    $('#converter_local').keyup(convert_to_ldoge);
+
 });
